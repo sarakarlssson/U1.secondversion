32c49db46b5234d86ca136f1df59a5f55892fd05
sarakarlssson <sara.amanda.karlsson@gmail.com>
2024-11-22 11:05:22 +0100

    Göra upp en struktur/plan
    Jag skriver i vilka steg jag behöver göra min uppgift för att få en överblick över hur jag ska börja och strukturera upp koden

---
+++ index.js
@@ -0,0 +1,21 @@
+
+// skapa alla cityBoxes
+
+// skapa table
+
+//userPrompt
+
+// loopa igenom databasen, se ifall userprompt namn finns i databasen
+
+// if userprompt finns = blir det vår targetCity variabel
+// sätta färg på targetCity
+//jämföra targetCityId med alla otherId
+//jämföra distance mellan targetCityDistance och infinity och hitta minsta distance
+// om distance är mindre så ska vi ha closestCityId variabel
+//när vi sedan har slutliga closestCityId använder vi Id för att ta fram dess namn i databasen
+//tillsätta class till closestCityId som ändrar färg + text hur nära den är targetCity
+
+//sedan gör vi samma sak fast med furthestCity.
+
+//          else ändrar vi rubriken till att userprompt inte finns i databasen + title på hemsidan
+
